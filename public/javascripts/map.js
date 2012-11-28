4e7478064be42e666905c05ddc0c3520fbaa03f9
Ryan Orban <ryan@ryanorban.com>
2012-11-27 21:45:30 -0800

    Removed GET call for records

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -19,36 +19,31 @@ $(document).ready(function() {
 
   map.locate({setView: true, maxZoom: 12});
 
-
-  $.get("/geo", function(result) {
-    for (var key in result) {
-      if (result.hasOwnProperty(key)) {
-        var LAT = result[key].coords.lat;
-        var LONG = result[key].coords.long;
-        L.marker(new L.LatLng(LAT, LONG)).bindPopup(result[key].comment).openPopout().addTo(map);
-      }
-    }
-  });
-  
-  
-
   function onLocationFound(e) {
       var radius = e.accuracy / 2;
+      var curLocation = e.latlng;
+      var curLatitude = curLocation.lat;
+      var curLongitude = curLocation.lng;
+      
+      // Add marker for current location
+      
+      var curLocationMarker = new L.Marker(curLocation);
+      map.addLayer(curLocationMarker);
+      curLocationMarker.bindPopup("Current Location").openPopup();
+      
+      // Add accuracy radius around current location
+      
+      var curLocationRadius = new L.Circle(curLocation, radius);
+      map.addLayer(curLocationRadius);
+      curLocationRadius.bindPopup("You are within " + radius + " meters from this point")
 
-      var uMarker = new L.Marker(e.latlng);
-      map.addLayer(uMarker);
-      uMarker.bindPopup("Current Location").openPopup();
-      var uCircle = new L.Circle(e.latlng, radius);
-      map.addLayer(uCircle);
-      uCircle.bindPopup("You are within " + radius + " meters from this point")
-
+      // POST current location to get records within N miles
  
-      $.post("/geo", {coords: {long: e.latlng.lng, lat: e.latlng.lat  }}, function(result) {
+      $.post("/geo", {coords: {long: curLongitude, lat: curLatitude  }}, function(result) {
           for (var key in result) {
             if (result.hasOwnProperty(key)) {
               var LAT = result[key].coords.lat;
               var LONG = result[key].coords.long;
-              console.log("Adding map marker: LAT: " + LAT + " LONG: " + LONG);
               L.marker(new L.LatLng(LAT, LONG)).bindPopup(result[key].comment).openPopup().addTo(map);
             };
           };
